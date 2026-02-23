a4e85a5de3bf998e617f429523e093602b7a57de
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-02-23 15:39:49 +0530

    Stage 1 practice commit

--- GitTest2/ViewController.swift
+++ GitTest2/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var MyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        MyLabel.textColor = .red
     }
 
 
