3dcdba47afd1c907c7a6f1e8e5dd481c06266d7e
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-02-23 15:02:00 +0530

    Initial commit

---
+++ GitTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest2
+//
+//  Created by Tharik Batcha on 23/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitTest2UITests/GitTest2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  GitTest2UITestsLaunchTests.swift
+//  GitTest2UITests
+//
+//  Created by Tharik Batcha on 23/02/26.
+//
+
+import XCTest
+
+final class GitTest2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
